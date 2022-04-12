861bbc76315dfe32a045f36e4d20a6ec8c9d18fb
JoseNSoler <josenico1212@gmail.com>
2022-04-12 17:53:21 -0500

    rabbit

---
+++ src/main/java/com/example/demo/StringerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringerApplication.class, args);
+	}
+
+}
